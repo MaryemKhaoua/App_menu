d255178eff1b0dae81c841256d4fcd6e13fd37d9
Maryem Khaoua <maryem.khaoua@gmail.com>
2023-09-18 12:11:53 +0100

    fixed error

---
+++ README.md
@@ -0,0 +1,39 @@
+Dans le cadre de votre travaille comme white hat hacker, vous voulez exploite le compte d’un utilisateur de facebook, et votre idée est créer une wordlist à partir les données de votre victime.
+
+Votre mission est réalisée cette application que génère le wordlist basant sur les données de la victime, Le menu de votre application comme suivant :
+
+==================================================================================
+
+                Menu d’application
+
+ ==================================================================================
+
+[1] afficher les données
+
+[2] ajouter une donnée
+
+[3] rechercher sur une donnée
+
+[4] supprimer une donnée
+
+[5] supprimer les données répètes
+
+[6] génère le wordlist
+
+[7] génère le wordlist dans un fichier .txt  // Un bonus
+
+[8] afficher les données de type entier // Un bonus
+
+ ==================================================================================
+
+ Tapez votre choix [1-6] : 
+
+ 
+
+ 
+
+Wordlist généré, il doit continu :
+
+-          Un, deux ou trois mot ajouter par utilisateur
+-          Avec des symboles comme ‘@$&#’
+-          Fais l’attention, les mots générés il doit pas répétée
---
+++ main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_WORDS 200
+#define MAX_WORD_LENGTH 70
+
+int i, count = 0;
+char words[MAX_WORDS][MAX_WORD_LENGTH];
+char word[MAX_WORD_LENGTH];
+
+void aff_data()
+{
+        printf("les donnees: \n");
+        for (i = 0; i < count; i++)
+
+                printf("%d: %s\n", i + 1, words[i]);
+}
+void add_data()
+{
+    if (count >= MAX_WORDS) {
+        printf("Impossible d'ajouter plus de mots, la liste est pleine\n");
+        return;
+    }
+
+    printf("ajouter un mot: ");
+    scanf("%s", words[count]);
+    count++;
+    printf("word ajoute avec succes\n");
+}
+void search_data()
+{
+
+	printf("entrez un mot a rechercher :  \n");
+	scanf("%s", word);
+
+	for (i = 0; i < count; i++)
+	{
+		if (strcmp(words[i], word) == 0)
+        {
+             printf("votre mot se trouve ici %d: \n", i + 1);
+             return;
+        } 
+    }
+		
+           
+		
+			printf("mot introuvable ici \n");
+	}
+int main()
+{
+    int choix;
+  while(1) 
+    {
+    printf("==================================================================================\n");
+    printf("                                Menu d'application\n");                                          
+    printf("==================================================================================\n");
+    printf("[1] afficher les donnees\n");
+    printf("[2] ajouter une donnee\n");
+    printf("[3] rechercher sur une donn%Ce\n", 130);
+    printf("[4] supprimer une donn%Ce\n", 130);
+    printf("[5] supprimer les donn%Ces r%Cp%Ctes\n", 130, 130, 138);
+    printf("[6] g%Cn%Cre le wordlist\n", 130, 138);
+    printf("[7] quitter\n");
+    printf("==================================================================================\n");
+    printf("Entrer votre choix: ");
+    scanf("%d", &choix);
+
+
+    switch (choix)
+    {
+        case 1: aff_data();
+break;
+	case 2: add_data();
+break;
+    case 3: search_data();
+break;
+    }
+    }
+    return (0);
+}
